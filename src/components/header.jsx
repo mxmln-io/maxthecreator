02c37b6b7ddbc4dddfd07ab15ef05098df2eaee8
Max-The-Creator <tranmaximillian@gmail.com>
2023-11-28 09:47:46 -0800

    update links and contact

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -14,7 +14,7 @@ export const Header = (props) => {
                 </h1>
                 <p>{props.data ? props.data.paragraph : "Loading"}</p>
                 <a
-                  href="https://web.getsquire.com/book/mysterieux-san-jose/barber/maximillian-kwun-tran/services"
+                  href="https://getsqr.co/maximillian-kwun-tran-1"
                   className="btn btn-custom btn-lg page-scroll"
                 >
                   Appointments
